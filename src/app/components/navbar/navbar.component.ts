2203bdfad631839fbd5e64711fa9bed298bb1f0c
JuanDango <juandancas@gmail.com>
2024-01-04 12:56:31 -0500

    Multiples overlays

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { Component, signal } from '@angular/core';
 import { BtnComponent } from '../btn/btn.component';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faC, faClose } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faInfoCircle, faClose } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -12,10 +12,17 @@ import { faC, faClose } from '@fortawesome/free-solid-svg-icons';
 })
 export class NavbarComponent {
   isOpen = signal<boolean>(false)
+  isOpenBody = signal<boolean>(false)
 
   faClose = faClose
+  faBell = faBell
+  faInfoCircle = faInfoCircle
 
   changeIsOpen(){
     this.isOpen.set(!this.isOpen())
+  }
+
+  changeIsOpenBody(){
+    this.isOpenBody.set(!this.isOpenBody())
   }
 }
