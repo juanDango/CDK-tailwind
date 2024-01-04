29a0e80307efa02bcdacbafb7183c8975f20e5b9
JuanDango <juandancas@gmail.com>
2024-01-04 12:40:15 -0500

    Overlays

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { BtnComponent } from '../btn/btn.component';
-import { OverlayModule } from '@angular/cdk/overlay'
+import { OverlayModule } from '@angular/cdk/overlay';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faC, faClose } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [BtnComponent, OverlayModule],
+  imports: [BtnComponent, OverlayModule, FontAwesomeModule],
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
   isOpen = signal<boolean>(false)
+
+  faClose = faClose
 
   changeIsOpen(){
     this.isOpen.set(!this.isOpen())
