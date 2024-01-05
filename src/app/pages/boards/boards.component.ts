29071bc1d5d71bafece0aabe531a0a4fd83bfb2c
JuanDango <juandancas@gmail.com>
2024-01-05 12:24:33 -0500

    acordeon

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -2,15 +2,24 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
-import { faGopuram, faHome, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faAngleUp, faBorderAll, faGear, faGopuram, faHeart, faHome, faPlus, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { faClock } from '@fortawesome/free-regular-svg-icons';
 import { BtnComponent } from '../../components/btn/btn.component';
 import { BoardComponent } from '../../components/board/board.component';
+import { CdkAccordionModule } from '@angular/cdk/accordion'
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent, FontAwesomeModule, BtnComponent, BoardComponent],
+  imports: [
+    NavbarComponent,
+    FontAwesomeModule,
+    BtnComponent,
+    BoardComponent,
+    CdkAccordionModule,
+    NgClass
+  ],
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent {
@@ -19,6 +28,11 @@ export class BoardsComponent {
   faHome = faHome
   faGopuram = faGopuram
   faPlus = faPlus
-
+  faAngleUp = faAngleUp
+  faAngleDown = faAngleDown
   faClock = faClock
+  faHeart = faHeart
+  faBorderAll = faBorderAll
+  faUsers = faUsers
+  faGear = faGear
 }
